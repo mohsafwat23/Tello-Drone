90b8d527997a79a442d990a331ff6420fcf7579d
Mohamed Safwat <safwat@wisc.edu>
2021-12-03 23:38:34 -0600

    added integral control

--- laptopAruco.py
+++ laptopAruco.py
@@ -6,24 +6,23 @@ from djitellopy import tello
 from time import *
 import csv
 
-me = tello.Tello()#beforeze drone 
-me.connect()#connect to the drone
-print(me.get_battery())
-me.streamon()
-me.takeoff()
-me.send_rc_control(0, 0, 25, 0) #go up at a speed of 25 cm/s for 1.5 seconds
-sleep(1.5)
+# me = tello.Tello()#initialze drone 
+# me.connect()#connect to the drone
+# print(me.get_battery())
+# me.streamon()
+# me.takeoff()
+# me.send_rc_control(0, 0, 25, 0) #go up at a speed of 25 cm/s for 1.5 seconds
+# sleep(1.5)
 
 
 
 pidX = [0.15, 0.1,0]
-pidY = [0.15, 0.4,0.2]
+pidY = [0.5, 0.4,0.2]
 pid_theta = [0.4, 0.4, 0]
 pError_theta = 0
 pErrorX = 0
 pErrorY = 0
-#w, h = 540, 340
-w, h = 960, 720
+w, h = 900, 600
 
 
 def findArUco(img, markerSize= 4, totalMarkers = 250, draw =True):
@@ -42,8 +41,8 @@ def findArUco(img, markerSize= 4, totalMarkers = 250, draw =True):
     #if ids != None:
         #cameraMatrix = np.array([[921.170702, 0.000000, 459.904354], [0.000000, 919.018377, 351.238301], [0.000000, 0.000000, 1.000000]])
         #distCoeffs = np.array([-0.033458, 0.105152, 0.001256, -0.006647, 0.000000])
-        cameraMatrix = np.array([[4.20160861e+03, 0.000000, 6.82522572e+02], [0.000000, 4.37676882e+03, 3.72848172e+02], [0.000000, 0.000000, 1.000000]])
-        distCoeffs = np.array([3.55373130e-01, -2.29822225e+01, 5.45797079e-03, -4.05893858e-03, -1.13433864e+00])
+        cameraMatrix = np.array([[1070.7148775074063, 0.000000, 525.967705495989], [0.000000, 1071.0529493852516, 360.77368834556813], [0.000000, 0.000000, 1.000000]])
+        distCoeffs = np.array([-0.10919219311566612, 2.5119183355562478, -0.006027127624687258, 8.475322686476765e-05, -17.6619436608614])
         rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, markerSize, cameraMatrix, distCoeffs)
         x_sum = corners[0][0][0][0]+ corners[0][0][1][0]+ corners[0][0][2][0]+ corners[0][0][3][0]
         y_sum = corners[0][0][0][1]+ corners[0][0][1][1]+ corners[0][0][2][1]+ corners[0][0][3][1]
@@ -64,7 +63,7 @@ def findArUco(img, markerSize= 4, totalMarkers = 250, draw =True):
         #if the marker is not detected return everything as 0/not detected
         return 0,0,0,0,0
 
-def trackMarker(me, info, pErrorY, pErrorX, pError_theta, speed_dn):
+def trackMarker(info, pErrorY, pErrorX, pError_theta, speed_dn):
     """ This function is used to track the marker and return the velocity error terms of the drone."""
 
     #pidY = [0.45, 0.3]
@@ -74,6 +73,7 @@ def trackMarker(me, info, pErrorY, pErrorX, pError_theta, speed_dn):
     errorX = x - w//2
     integralX = integral[0] + (errorX * dt)
     integralY = integral[1] + (errorY * dt)
+    print("dt:", dt)
     speed_lr = pidX[0]*errorX + pidX[1]*(errorX-pErrorX)+ pidX[2]*integralX
     speed_fb = pidY[0]*errorY + pidY[1]*(errorY-pErrorY)+pidY[2]*integralY
     speed_yaw = pid_theta[0]*error_theta + pid_theta[1]*(error_theta-pError_theta)
@@ -93,40 +93,39 @@ def trackMarker(me, info, pErrorY, pErrorX, pError_theta, speed_dn):
         speed_yaw = 0
         error_theta = 0
     #print(errorX, errorY)
-    me.send_rc_control(speed_lr, speed_fb, speed_dn, speed_yaw)# add speed l_r
+    #me.send_rc_control(speed_lr, speed_fb, speed_dn, speed_yaw)# add speed l_r
     return errorY,errorX,error_theta,speed_lr,speed_fb,area
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-t_before = time()
+t_initial = time()
 dt = 0
 integral = [0,0]
 f = open("ErrorInY.csv", "a+")
 writer = csv.writer(f, delimiter=',')
 while True:
-    t_initial = time()-t_before
-    img = me.get_frame_read().frame
+    t = time()-t_initial
+    #img = me.get_frame_read().frame
+    _, img = cap.read()
     #_, img = cap.read()
     img=cv2.resize(img,(w,h))
     info = findArUco(img)
-    if int(t_final) > 10:
-        #if the specfied time has passed, then the drone will slowly descend
-        pErrorY,pErrorX,pError_theta,speed_lr,speed_fb,area = trackMarker(me,info, pErrorY, pErrorX, pError_theta,speed_dn = -1)
+    if int(t) > 10:
+        pErrorY,pErrorX,pError_theta,speed_lr,speed_fb,area = trackMarker(info, pErrorY, pErrorX, pError_theta,speed_dn = -1)
         #print(area)
         if area > 350:
-            #if the area of the marker relative to the camera frame is greater than 350, then the drone will land
-            me.land()
+            pass
     else:
-        pErrorY,pErrorX,pError_theta,speed_lr,speed_fb,area = trackMarker(me,info, pErrorY, pErrorX, pError_theta, speed_dn = 0)
-    t_final = time()-t_before
-    data = [t_final,pErrorY,0]
-    dt = t_final-t_initial 
-    print(1/dt) #frequency
+        pErrorY,pErrorX,pError_theta,speed_lr,speed_fb,area = trackMarker(info, pErrorY, pErrorX, pError_theta, speed_dn = 0)
+        #print(area)
+    print("ErrorY:", pErrorY)
+    print("speed_fb:", speed_fb)
+    data = [t,pErrorY,0]
+    t_final = time()-t_initial
+    dt = t_final-t
+    #print(1/dt)
     cv2.imshow("Output",img)
     writer.writerow(data)
     f.flush()
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        me.land()
-        print(me.get_battery())
-        me.streamoff()
         break
      
